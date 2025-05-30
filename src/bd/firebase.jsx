5c42d8dd1daa7fab8a24c2325cbd89c86bb9b336
Sebastian <sebastian@gmail.com>
2025-05-30 00:42:36 -0500

    update

--- src/bd/firebase.jsx
+++ src/bd/firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: FIRESTORE_API_KEY,
-  authDomain: FIRESTORE_AUTH_DOMAIN,
-  projectId: FIRESTORE_PROJECT_ID,
-  storageBucket: FIRESTORE_STORAGE_BUCKET,
-  messagingSenderId: FIRESTORE_MESSAGIN_SENDER_ID,
-  appId: FIRESTORE_APP_ID
+  apiKey: process.env.FIRESTORE_API_KEY,
+  authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRESTORE_MESSAGIN_SENDER_ID,
+  appId: process.env.FIRESTORE_APP_ID,
 };
 
 
